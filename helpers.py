f923b08f99afbd6736eee7a9495d68add8e849a8
Douglasjp66 <83411148+Douglasjp66@users.noreply.github.com>
2021-07-07 10:03:42 -0300

    criada funções de total_tentativas,verificação  randomica de palavra secreta, status de acertos e tentativas

--- helpers.py
+++ helpers.py
@@ -1,14 +1,48 @@
 import random
 from config import TENTATIVAS_ADICIONAIS,ARQUIVO_PALAVRAS_SECRETAS
-def gerar_palav_secreta ():
+def gerar_palavra_secreta ():
     """
-    Função de seleção randômica de palavras no arquivo txt
+    Função de seleção randômica de palavras do arquivo txt
     :return: uma palavra aleatória
     """
     with open(ARQUIVO_PALAVRAS_SECRETAS,'r') as f_obj:
         palavra = f_obj.read().splitlines()
 
     return random.choice(palavra)
+ def verificar_palavra_randomica (palavra_secreta,suas_tentativas,tentativas):
+   """
+   Verifica se a letra dada está correta
+   :param palavra_secreta: Gerada co base nos arquivo palavras secretas
+   :param suas_tentativas: Lista com todas as tentativas
+   :param tentativas: letra inserida nessa jogada
+   :return: retorna um status - acerto ou erro
+   """
+   status= '' # O status precisa ser zerado a cada chamada da função
+   acertos='' # Acertos precisa ser zerado a cada tentativa/jogada
+
+   for letra in palavra_secreta:
+       if letra.lower() in suas_tentativas:
+           status += letra
+       else:
+           status += '*'
+
+       if letra.lower() == tentativa.lower()
+           acertos += 1
+
+   print(f"\nAcertou {acertos} letra(s), {'tentativa'}")
+   return status
+
+ def total_tentativas(palavra_secreta):
+     """
+     Define o total de tentativas de acordo a palavra secreta
+     :param palavra_secreta: gerala aleatoriamente
+     :return: quantidade de tentativas
+     """
+     chences = len(palavra_secreta)
+     return chances + TENTATIVAS_ADICIONAIS
+
+
+
 
 
 
